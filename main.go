8851e9e89c7cd3c42f3e76789a858d80751bf95b
gerard <you@example.com>
2017-04-10 16:11:05 +1200

    Removed marshalling from controller

--- main.go
+++ main.go
@@ -14,44 +14,45 @@ func main() {
 	r := gin.Default()
 
 	ticketTypesResponseData := readResponseData("TicketTypesResponse")
+	tt := make([]interface{}, 0)
+	tt = unmarshalJsonArray(ticketTypesResponseData, tt)
+
 	orderCreatedResponseData := readResponseData("OrderCreatedResponse")
+	var oc interface{}
+	unmarshalJsonObject(orderCreatedResponseData, &oc)
+
 	checkoutResponseData := readResponseData("CheckoutResponse")
+	var co interface{}
+	unmarshalJsonObject(checkoutResponseData, &co)
 
 	r.GET("/RESTData.svc/cinemas/:cinema/sessions/:session/tickets", func(c *gin.Context) {
-		tt := make([]interface{}, 0)
-		if err := json.Unmarshal(ticketTypesResponseData, &tt); err != nil {
-			fmt.Printf("error: %v\n", err)
-		}
-
-		fmt.Printf("Results: %v\n", tt)
 		c.JSON(200, tt)
 	})
 
 	r.POST("/RESTTicketing.svc/order/tickets", func(c *gin.Context) {
-		var oc interface{};
-		if err := json.Unmarshal(orderCreatedResponseData, &oc); err != nil {
-			fmt.Printf("error: %v\n", err)
-		}
-
-		fmt.Printf("Results: %v\n", oc)
 		c.JSON(200, oc)
 	})
 
 	r.POST("/RESTTicketing.svc/order", func(c *gin.Context) {
-		var co interface{};
-		if err := json.Unmarshal(checkoutResponseData, &co); err != nil {
-			fmt.Printf("error: %v\n", err)
-		}
-
-		fmt.Printf("Results: %v\n", co)
 		c.JSON(200, co)
 	})
 
 	// Listen and Server in 0.0.0.0:8080
 	r.Run(":8080")
 }
+func unmarshalJsonArray(ticketTypesResponseData []byte, arr []interface{}) []interface{} {
+	if err := json.Unmarshal(ticketTypesResponseData, &arr); err != nil {
+		fmt.Printf("Error: %v\n", err)
+	}
+	return arr
+}
 
-
+func unmarshalJsonObject(ticketTypesResponseData []byte, obj interface{}) interface{} {
+	if err := json.Unmarshal(ticketTypesResponseData, &obj); err != nil {
+		fmt.Printf("Error: %v\n", err)
+	}
+	return obj
+}
 
 func readResponseData(responseFileName string) []byte {
 	responseData, e := ioutil.ReadFile("/tmp/" + responseFileName)
